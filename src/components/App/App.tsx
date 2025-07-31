a3c4f35035cd7217b22c01cb83838e71de65ece8
Oleksandr <rachok010291@gmail.com>
2025-07-31 20:35:35 +0100

    first commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,2 @@
+import { useState } from "react";
+import css from "./App.module.css";
---
+++ src/main.tsx
@@ -0,0 +1,4 @@
+import { createRoot } from "React-dom/client";
+import App from "./components/App/App";
+
+createRoot(document.getElementById("root") as HTMLElement).render(<App />);
